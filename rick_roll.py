5b406d7545871b91f5bf163d6b1b805250fc2ddd
ZeroTwoModz <70921101+ZeroTwoModz@users.noreply.github.com>
2021-08-04 23:49:19 +0400

    Update rick_roll.py

--- rick_roll.py
+++ rick_roll.py
@@ -7,6 +7,7 @@ ASCII_CHARS = ['⠀','⠄','⠆','⠖','⠶','⡶','⣩','⣪','⣫','⣾','⣿'
 ANOTHER ASCII CHARS
 
 ASCII_CHARS = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~i!lI;:,\"^`"
+ASCII_CHARS = " @MB█▒****"
 '''
 
 
