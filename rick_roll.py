db49c0fb1e73bb68fe62ff30b8bf72e8f962aa65
ZeroTwoModz <70921101+ZeroTwoModz@users.noreply.github.com>
2021-08-04 23:06:30 +0400

    added sum stuff
    hi

--- rick_roll.py
+++ rick_roll.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import time
 import json
+import os
 ASCII_CHARS = ['⠀','⠄','⠆','⠖','⠶','⡶','⣩','⣪','⣫','⣾','⣿']
 
 def resized_gray_image(image ,new_width=70):
@@ -19,7 +20,7 @@ def generate_frame(image,new_width=70):
 	new_image_data = pix2chars(resized_gray_image(image,new_width=new_width))
 	total_pixels = len(new_image_data)
 	output = "\n".join([new_image_data[index:(index+new_width)] for index in range(0, total_pixels, new_width)])
-	return "```"+output+"\n```"
+	return output
 
 with open('config.json') as penis:
     data = json.load(penis)
@@ -40,17 +41,17 @@ async def start(ctx):
 		i = 0
 		isCreated = False
 		msg = None
-		while i < 3786:
+		while i < 4053:
 			i = i + 3
 			img = Image.open(f"frames/frame{i}.jpg")
-			frame = generate_frame(img,60)
+			frame = generate_frame(img,60);
 			if frame != None:
 				if isCreated == False:
-					msg = await ctx.send(frame)
+					msg = await ctx.send("```"+frame+"```"); os.system('cls' if os.name=='nt' else 'clear'); print(frame+"\n\n\nzerotwo n1")
 					isCreated = True
 					time.sleep(0.1)
 				else:
-					await msg.edit(content=frame)
+					await msg.edit(content="```"+frame+"```"); os.system('cls' if os.name=='nt' else 'clear'); print(frame+"\n\n\nzerotwo n1")
 					time.sleep(0.5)
 					
 		await ctx.send(".....................................................")
@@ -58,8 +59,3 @@ async def start(ctx):
 
 
 rick.run(data["token"], bot=False)
-
-
-
-
-	
